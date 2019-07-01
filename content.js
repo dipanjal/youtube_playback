57f6e944d8b940bf77464f2ae85e5b7d1c61dce3
dipanjal <dipanjalmaitra@gmail.com>
2019-07-01 13:21:30 +0600

    video-end eventListener Added

--- content.js
+++ content.js
@@ -1,20 +1,17 @@
 var isPlaying = true;
 // var wait = false;
-
+// console.log('content script')
 window.addEventListener('load',event=>{
-
-    
-    // injectHTML(document);
-
+	// console.log('windown loaded')
     chrome.storage.onChanged.addListener(function(changes, namespace) {
 		var key = 'playlist';
 		var storageChange = changes[key];
 		if(storageChange){
 
-			if(storageChange.newValue && storageChange.newValue.length>0){
-				// wait=false;
-				isPlaying = true;
-			}
+			// if(storageChange.newValue && storageChange.newValue.length>0){
+			// 	// wait=false;
+			// 	isPlaying = true;
+			// }
 			
 			console.log('Storage key "%s" in namespace "%s" changed. ' +
                         'Old value was "%s", new value is "%s".',
@@ -26,44 +23,67 @@ window.addEventListener('load',event=>{
 
 	});
 	
-	monitorYTPlayer(document);
-});
+	// monitorYTPlayer(document);
 
-function monitorYTPlayer(document_root) { 
-	var totalTimeTag = document_root.getElementsByClassName("ytp-time-duration")[0];
-	if(totalTimeTag){
-		var totalTime = totalTimeTag.innerHTML;
-		setInterval(function(){
-			console.log("isPlaying... "+isPlaying);
-			if(isPlaying){
-				var playPauseButton = document_root.querySelector(".ytp-play-button.ytp-button");
-				var playerState =  playPauseButton.getAttribute("aria-label");
-				var watchTime = document_root.getElementsByClassName("ytp-time-current")[0].innerHTML
-				if(watchTime===totalTime){
-					// wait=true;
-					isPlaying = false;
-					try{
-						chrome.storage.sync.get('playlist', (result) => {
-							if(result.playlist){
-								var playListArr = JSON.parse(result.playlist);
-								if(playListArr.length>0){
-									// console.log("Travars Next Track");
-									changeTrack(playListArr);
-								}else{
-									// wait = false;
-								}
-							}
-						});	
-					}catch(err){
-						console.log(err);
+	var videoTag = document.querySelector('video');
+	if(videoTag){
+		console.log('video player found');
+		videoTag.addEventListener('ended',function(){
+			// console.log('ended')
+			try{
+				chrome.storage.sync.get('playlist', (result) => {
+					if(result.playlist){
+						var playListArr = JSON.parse(result.playlist);
+						if(playListArr.length>0){
+							changeTrack(playListArr);
+						}
 					}
-					
-				}
+				});	
+			}catch(err){
+				console.log(err);
 			}
 			
-		},1000);
+		});
 	}
-}
+
+});
+
+// function monitorYTPlayer(document_root) {
+// 	console.log('Monitor YTPlayer')
+// 	var totalTimeTag = document_root.getElementsByClassName("ytp-time-duration")[0];
+// 	if(totalTimeTag){
+// 		var totalTime = totalTimeTag.innerHTML;
+// 		setInterval(function(){
+// 			console.log("isPlaying... "+isPlaying);
+// 			if(isPlaying){
+// 				var playPauseButton = document_root.querySelector(".ytp-play-button.ytp-button");
+// 				var playerState =  playPauseButton.getAttribute("aria-label");
+// 				var watchTime = document_root.getElementsByClassName("ytp-time-current")[0].innerHTML
+// 				if(watchTime===totalTime){
+// 					// wait=true;
+// 					isPlaying = false;
+// 					try{
+// 						chrome.storage.sync.get('playlist', (result) => {
+// 							if(result.playlist){
+// 								var playListArr = JSON.parse(result.playlist);
+// 								if(playListArr.length>0){
+// 									// console.log("Travars Next Track");
+// 									changeTrack(playListArr);
+// 								}else{
+// 									// wait = false;
+// 								}
+// 							}
+// 						});	
+// 					}catch(err){
+// 						console.log(err);
+// 					}
+					
+// 				}
+// 			}
+			
+// 		},1000);
+// 	}
+// }
 
 
 
@@ -75,7 +95,7 @@ function changeTrack(playListArr){
 		console.log('playlist updated');
 		console.log(dataSerialized);
 		// wait = false;
-		isPlaying = true;
+		// isPlaying = true;
 		// Simulate a mouse click:
 		window.location.href = nextTrack;
 	});
